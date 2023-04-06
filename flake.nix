fe10087e948fd5e1c3dfb15df126c453724d053e
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-04-06 07:09:25 -0700

    fix: Update nix version with release-please

--- flake.nix
+++ flake.nix
@@ -51,7 +51,9 @@
 
       commonArgs = {
         pname = "barretenberg-sys";
-        version = "0.1.0";
+        # x-release-please-start-version
+        version = "0.0.0";
+        # x-release-please-end
 
         # As per https://discourse.nixos.org/t/gcc11stdenv-and-clang/17734/7 since it seems that aarch64-linux uses
         # gcc9 instead of gcc11 for the C++ stdlib, while all other targets we support provide the correct libstdc++

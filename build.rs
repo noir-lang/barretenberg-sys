4b2496c137553045e6cac6ca3e469853711b9fde
kevaundray <kevtheappdev@gmail.com>
2023-05-18 23:26:27 +0000

    include automatically generated bindings and link to static-libs folder.
    Note: the path is using `/mnt/user-data/kev/`

--- build.rs
+++ build.rs
@@ -37,73 +37,89 @@ fn main() -> Result<()> {
         .into_hooks();
     eyre_hook.install()?;
 
-    let lib = pkg_config::Config::new()
-        .range_version("0.1.0".."0.2.0")
-        .probe("barretenberg")
-        .map_err(|err| match err {
-            Error::EnvNoPkgConfig(val) => BuildError::PkgConfigDisabled(val),
-            Error::ProbeFailure {
-                name: _,
-                command: _,
-                ref output,
-            } => BuildError::PkgConfigProbe(
-                String::from_utf8_lossy(&output.stderr).trim().to_string(),
-            ),
-            err => BuildError::PkgConfigGeneric(format!("{err}")),
-        })?;
+    // Turned off multi-threading in BB so we can ignore libomp
+    // link_lib_omp();
 
-    link_lib_omp();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        "/mnt/user-data/kev/barretenberg-sys/static-libs"
+    );
 
-    let include_args = lib
-        .include_paths
-        .iter()
-        .map(|include| format!("-I{}", include.to_string_lossy()));
+    // #[cfg(target_family = "unix")]
+    // println!("cargo:rustc-link-lib=static=barretenberg-linux");
+    // #[cfg(target_family = "wasm")]
+    // println!("cargo:rustc-link-lib=static=c");
+    // println!("cargo:rustc-link-lib=static=c++");
+    // println!("cargo:rustc-link-lib=static=c++abi");
+    // println!("cargo:rustc-link-lib=static=crt1-command");
+    println!("cargo:rustc-link-lib=static=barretenberg-wasm");
+
+    cc::Build::new().archiver("llvm-ar").cpp_link_stdlib(None);
+
+    // let lib = pkg_config::Config::new()
+    //     .range_version("0.1.0".."0.2.0")
+    //     // .probe("barretenberg")
+    //     .map_err(|err| match err {
+    //         Error::EnvNoPkgConfig(val) => BuildError::PkgConfigDisabled(val),
+    //         Error::ProbeFailure {
+    //             name: _,
+    //             command: _,
+    //             ref output,
+    //         } => BuildError::PkgConfigProbe(
+    //             String::from_utf8_lossy(&output.stderr).trim().to_string(),
+    //         ),
+    //         err => BuildError::PkgConfigGeneric(format!("{err}")),
+    //     })?;
+
+    // let include_args = lib
+    //     .include_paths
+    //     .iter()
+    //     .map(|include| format!("-I{}", include.to_string_lossy()));
 
     // Generate bindings from a header file and place them in a bindings.rs file
-    let bindings = bindgen::Builder::default()
-        // Clang args so that we can compile C++ with C++20
-        .clang_args(&["-std=gnu++20", "-xc++"])
-        .clang_args(include_args)
-        .header_contents(
-            "wrapper.hpp",
-            r#"
-            #include <barretenberg/dsl/acir_proofs/c_bind.hpp>
-            #include <barretenberg/crypto/blake2s/c_bind.hpp>
-            #include <barretenberg/crypto/pedersen_commitment/c_bind.hpp>
-            #include <barretenberg/crypto/schnorr/c_bind.hpp>
-            #include <barretenberg/ecc/curves/bn254/scalar_multiplication/c_bind.hpp>
-            "#,
-        )
-        .allowlist_function("blake2s_to_field")
-        .allowlist_function("acir_proofs_get_solidity_verifier")
-        .allowlist_function("acir_proofs_get_exact_circuit_size")
-        .allowlist_function("acir_proofs_get_total_circuit_size")
-        .allowlist_function("acir_proofs_init_proving_key")
-        .allowlist_function("acir_proofs_init_verification_key")
-        .allowlist_function("acir_proofs_new_proof")
-        .allowlist_function("acir_proofs_verify_proof")
-        .allowlist_function("pedersen_plookup_compress_fields")
-        .allowlist_function("pedersen_plookup_compress")
-        .allowlist_function("pedersen_plookup_commit")
-        .allowlist_function("new_pippenger")
-        .allowlist_function("compute_public_key")
-        .allowlist_function("construct_signature")
-        .allowlist_function("verify_signature")
-        .generate()
-        .map_err(|err| match err {
-            BindgenError::ClangDiagnostic(msg) => {
-                BuildError::BindgenErrorClangDiagnostic(msg.trim().to_string())
-            }
-            err => BuildError::BindgenGeneric(format!("{err}").trim().to_string()),
-        })?;
+    // let bindings = bindgen::Builder::default()
+    //     // Clang args so that we can compile C++ with C++20
+    // .clang_args(&["-std=gnu++20", "-xc++"])
+    //     // .clang_args(include_args)
+    //     .header_contents(
+    //         "wrapper.hpp",
+    //         r#"
+    //         #include <barretenberg/dsl/acir_proofs/c_bind.hpp>
+    //         #include <barretenberg/crypto/blake2s/c_bind.hpp>
+    //         #include <barretenberg/crypto/pedersen_commitment/c_bind.hpp>
+    //         #include <barretenberg/crypto/schnorr/c_bind.hpp>
+    //         #include <barretenberg/ecc/curves/bn254/scalar_multiplication/c_bind.hpp>
+    //         "#,
+    //     )
+    //     .allowlist_function("blake2s_to_field")
+    //     .allowlist_function("acir_proofs_get_solidity_verifier")
+    //     .allowlist_function("acir_proofs_get_exact_circuit_size")
+    //     .allowlist_function("acir_proofs_get_total_circuit_size")
+    //     .allowlist_function("acir_proofs_init_proving_key")
+    //     .allowlist_function("acir_proofs_init_verification_key")
+    //     .allowlist_function("acir_proofs_new_proof")
+    //     .allowlist_function("acir_proofs_verify_proof")
+    //     .allowlist_function("pedersen_plookup_compress_fields")
+    //     .allowlist_function("pedersen_plookup_compress")
+    //     .allowlist_function("pedersen_plookup_commit")
+    //     .allowlist_function("new_pippenger")
+    //     .allowlist_function("compute_public_key")
+    //     .allowlist_function("construct_signature")
+    //     .allowlist_function("verify_signature")
+    // .generate()
+    // .map_err(|err| match err {
+    //     BindgenError::ClangDiagnostic(msg) => {
+    //         BuildError::BindgenErrorClangDiagnostic(msg.trim().to_string())
+    //     }
+    //     err => BuildError::BindgenGeneric(format!("{err}").trim().to_string()),
+    // })?;
 
-    println!("cargo:rustc-link-lib=static=barretenberg");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let bindgen_file = out_path.join("bindings.rs");
-    bindings
-        .write_to_file(&bindgen_file)
-        .map_err(|_| BuildError::BindgenWrite(bindgen_file.to_string_lossy().to_string()).into())
+    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    // let bindgen_file = out_path.join("bindings.rs");
+    Ok(())
+    // bindings
+    //     .write_to_file(&bindgen_file)
+    //     .map_err(|_| BuildError::BindgenWrite(bindgen_file.to_string_lossy().to_string()).into())
 }
 
 fn link_lib_omp() {
